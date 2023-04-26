16fbd9bb512bccadfca96471f007ffedb3f1044d
japheth91 <awesomejaphet@gmail.com>
2023-04-26 13:55:16 -0700

    task 0

--- _printf.c
+++ _printf.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
 
 /**
  * _printf- main function.
@@ -10,9 +12,46 @@ int _printf(const char *format, ...)
 	va_list arguments;
 
 	va_start(arguments, format);
-	int count = printf(format, arguments);
+	int count = 0;
+	int c;
+	char *s
+	
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+			{
+				c = va_arg(arguments, int);
+				putchar(c);
+				count++;			
+			}
+			else if (*format == 's')
+			{
+				*s = va_arg(arguments, char *);
+				while (*s != '0')
+				{
+					putchar(*s);
+					s++;
+					count++;
+				}
+				else if (*format == '%')
+				{
+					putchar('%');
+					count++;
+				}
+			}
+			else 
+			{
+				putchar(*format);
+				count++;
+			}
+		}
 
+	format++;
+	}
 	va_end(arguments);
-
 	return (count);
 }
+	
